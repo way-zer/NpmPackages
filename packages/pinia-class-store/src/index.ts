1cdde4b3227947b7bb8eee4eb6949d88b47535d2
way-zer <himc.wicp@gmail.com>
2022-10-23 14:58:58 +0800

    (pinia-class-store) fix Store type

--- packages/pinia-class-store/src/index.ts
+++ packages/pinia-class-store/src/index.ts
@@ -12,11 +12,18 @@ interface ModuleExt {
     }
 }
 
+type Actions<T extends Record<string, any>> = {
+    [P in keyof T as T[P] extends (...args: any[]) => any ? P : never]: T[P];
+};
+//we can't distinguish state and getter, so see getter as state for type
+type StateAndGetter<T extends Record<string, any>> = Omit<T, keyof Actions<T>>;
+
 /**
  * @param Module0 类名
  * @param id Store的id,可选，默认使用类名
  */
-export function useStore<T extends (new (...args) => any)>(Module0: T, id?: string): InstanceType<T> & Store<string, T, T, T> {
+export function useStore<T extends (new (...args) => any), G extends InstanceType<T> = InstanceType<T>>(Module0: T, id?: string)
+    : G & Store<string, StateAndGetter<G>, {}, Actions<G>> {
     const Module = Module0 as T & ModuleExt
     id = id || Module.name
     if (!Module._storeOptions) {
@@ -48,5 +55,5 @@ export function useStore<T extends (new (...args) => any)>(Module0: T, id?: stri
         getters, actions
     })()
     Object.setPrototypeOf(store, Module.prototype)
-    return store as InstanceType<T>
+    return store as G
 }
